f265ef060436c104f09b6b8c883441617c641788
FabioRocha2227 <92931270+FabioRocha2227@users.noreply.github.com>
2022-10-04 14:11:10 +0100

    Final Semana 3/4

---
+++ src/main/java/org/example/Game.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    public TerminalScreen screen;
+    private final Arena arena;
+
+    public Game(int width, int height) throws IOException {
+        Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(width, height)).createTerminal();
+        screen = new TerminalScreen(terminal);
+
+        screen.setCursorPosition(null);   // we don't need a cursor
+        screen.startScreen();             // screens must be started
+        screen.doResizeIfNecessary();     // resize screen if necessary
+
+        arena = new Arena(width, height);
+    }
+
+    private void draw() throws IOException {
+        screen.clear();
+        arena.draw(screen.newTextGraphics());
+        screen.refresh();
+    }
+
+    private void processKey(KeyStroke key) {
+        arena.processKey(key);
+    }
+
+    public void run() throws IOException {
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')
+                screen.close();
+            if (key.getKeyType() == KeyType.EOF)
+                break;
+            processKey(key);
+        }
+    }
+}
